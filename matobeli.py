f46183626a1623ebb9c21f82f7c4ada26817340e
dimitriizz <dimitrivw00@gmail.com>
2022-03-08 10:20:35 +0200

    about valmis peli enää pientä hieno säätöä

--- matobeli.py
+++ matobeli.py
@@ -5,45 +5,57 @@ import random
 
 pygame.init()
 
+
+#määritellää värikoodeille muuttujat
 white = (255, 255, 255)
-yellow = (255, 255, 255)
+yellow = (255, 255, 102)
 black = (0, 0, 0)
-red = (255, 0, 0)
+red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
+#näytömn koo'on muuttujat
 dis_width = 600
 dis_height = 400
 
+#da display
 dis=pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Mato Beli')
 
+#aika
 clock = pygame.time.Clock()
 
 snake_block = 10
 snake_speed = 15
 
+#fontti
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
+
+#fubktio pisteille
 def Your_score(score):
     value = score_font.render("Pisteesi: " + str(score), True, yellow)
     dis.blit(value, [0, 0])
 
+
+#funktio käärmeelle
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
+#funktio viestille
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
+#funktio peliloopille
 def gameLoop():
     game_over = False
     game_close =False
 
-    x1 = dis_width /2
-    y1 = dis_height /2
+    x1 = dis_width / 2
+    y1 = dis_height / 2
 
     x1_change = 0
     y1_change = 0
